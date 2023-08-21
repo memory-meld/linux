7db1fe74a120c15ec7e666ca566c3ea40c502d92
Junliang HU <jlhu@cse.cuhk.edu.hk>
2023-08-21 16:40:40 +0800

    mm/mempolicy: make symbol non-static: get_nodes

--- mm/mempolicy.c
+++ mm/mempolicy.c
@@ -1431,7 +1431,7 @@ static int get_bitmap(unsigned long *mask, const unsigned long __user *nmask,
 }
 
 /* Copy a node mask from user space. */
-static int get_nodes(nodemask_t *nodes, const unsigned long __user *nmask,
+int get_nodes(nodemask_t *nodes, const unsigned long __user *nmask,
 		     unsigned long maxnode)
 {
 	--maxnode;
