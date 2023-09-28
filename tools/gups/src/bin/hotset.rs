33f9b3170c946fef5fd40f77f18a1a01e23bc1fb
Junliang HU <jlhu@cse.cuhk.edu.hk>
2023-09-28 17:23:03 +0800

    tools/gups: gups benchmark implementation in rust

---
+++ tools/gups/src/bin/hotset.rs
@@ -0,0 +1,60 @@
+use std::time::Instant;
+
+use anyhow::Result;
+use clap::Parser;
+use mix_distribution::Mix;
+use rand::distributions::Uniform;
+use tracing::info;
+
+use gups::*;
+
+/// GUPS hotset version with `weight` times as more updates going to the hot region than to the rest.
+#[derive(Parser, Debug)]
+#[command(author, version, about)]
+struct Args {
+    /// Number of worker threads
+    threads: usize,
+    /// Number of updates per thread
+    updates: usize,
+    /// Length of the entire memory region
+    len: usize,
+    /// Granularity of each update
+    granularity: usize,
+    /// Length of the hot memory region
+    hot_len: usize,
+    /// Weight ratio of hot region to the rest
+    weight: usize,
+}
+
+fn main() -> Result<()> {
+    tracing_subscriber::fmt::init();
+    let args = Args::parse();
+    info!("gups args {args:?}");
+
+    let split = args.hot_len / args.threads / args.granularity;
+    let end = args.len / args.threads / args.granularity;
+    let dist = Mix::new(
+        [Uniform::new(0, split), Uniform::new(split, end)],
+        [args.weight, 1],
+    )
+    .unwrap();
+    let mut gups = Gups::new(args.threads, args.updates, args.len, args.granularity, dist)?;
+
+    // warm up
+    let start = Instant::now();
+    let handles = gups.start_workers()?;
+    for w in handles {
+        w.join().expect("failed wating for worker threads");
+    }
+    info!("warm up took: {:?}", start.elapsed());
+
+    // timed iteration
+    let start = Instant::now();
+    let handles = gups.start_workers()?;
+    for w in handles {
+        w.join().expect("failed wating for worker threads");
+    }
+    info!("timed iteration took: {:?}", start.elapsed());
+
+    Ok(())
+}
---
+++ tools/gups/src/bin/random.rs
@@ -0,0 +1,50 @@
+use std::time::Instant;
+
+use anyhow::Result;
+use clap::Parser;
+use rand::distributions::Uniform;
+use tracing::info;
+
+use gups::*;
+
+/// GUPS random version with all updates going to the entire region uniformly
+#[derive(Parser, Debug)]
+#[command(author, version, about)]
+struct Args {
+    /// Number of worker threads
+    threads: usize,
+    /// Number of updates per thread
+    updates: usize,
+    /// Length of the entire memory region
+    len: usize,
+    /// Granularity of each update
+    granularity: usize,
+}
+
+fn main() -> Result<()> {
+    tracing_subscriber::fmt::init();
+    let args = Args::parse();
+    info!("gups args {args:?}");
+
+    let end = args.len / args.threads / args.granularity;
+    let dist = Uniform::new(0, end);
+    let mut gups = Gups::new(args.threads, args.updates, args.len, args.granularity, dist)?;
+
+    // warm up
+    let start = Instant::now();
+    let handles = gups.start_workers()?;
+    for w in handles {
+        w.join().expect("failed wating for worker threads");
+    }
+    info!("warm up took: {:?}", start.elapsed());
+
+    // timed iteration
+    let start = Instant::now();
+    let handles = gups.start_workers()?;
+    for w in handles {
+        w.join().expect("failed wating for worker threads");
+    }
+    info!("timed iteration took: {:?}", start.elapsed());
+
+    Ok(())
+}
