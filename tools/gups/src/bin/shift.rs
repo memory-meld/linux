6e5e654a73882cc8021830ef09a579464f6cebd3
Junliang HU <jlhu@cse.cuhk.edu.hk>
2023-09-28 17:23:03 +0800

    tools/gups: gups benchmark implementation in rust

---
+++ tools/gups/src/bin/shift.rs
@@ -0,0 +1,76 @@
+use std::time::Instant;
+
+use anyhow::Result;
+use clap::Parser;
+use mix_distribution::Mix;
+use rand::distributions::Uniform;
+use tracing::info;
+
+use gups::*;
+
+/// GUPS shift version with `weight` times as more updates going to the hot region than to the rest.
+/// The hot region will start at the begnning of each thread's local memory region in the first
+/// timed iteraton. Then it will shift to the end of each thread's local memory region.
+#[derive(Parser, Debug)]
+#[command(author, version, about)]
+struct Args {
+    /// Number of worker threads
+    threads: usize,
+    /// Number of updates per thread
+    updates: usize,
+    /// Length of the entire memory region
+    len: usize,
+    /// Granularity of each update
+    granularity: usize,
+    /// Length of the hot memory region
+    hot_len: usize,
+    /// Weight ratio of hot region to the rest
+    weight: usize,
+}
+
+fn main() -> Result<()> {
+    tracing_subscriber::fmt::init();
+    let args = Args::parse();
+    info!("gups args {args:?}");
+
+    let split = args.hot_len / args.threads / args.granularity;
+    let end = args.len / args.threads / args.granularity;
+    let dist = Mix::new(
+        [Uniform::new(0, split), Uniform::new(split, end)],
+        [args.weight, 1],
+    )
+    .unwrap();
+    let mut gups = Gups::new(args.threads, args.updates, args.len, args.granularity, dist)?;
+
+    // warm up
+    let start = Instant::now();
+    let handles = gups.start_workers()?;
+    for w in handles {
+        w.join().expect("failed wating for worker threads");
+    }
+    info!("warm up took: {:?}", start.elapsed());
+
+    // timed iteration
+    let start = Instant::now();
+    let handles = gups.start_workers()?;
+    for w in handles {
+        w.join().expect("failed wating for worker threads");
+    }
+    info!("timed iteration took: {:?}", start.elapsed());
+
+    // shifted hotset
+    let dist = Mix::new(
+        [Uniform::new(0, end - split), Uniform::new(end - split, end)],
+        [1, args.weight],
+    )
+    .unwrap();
+    gups.reset_distribution(dist);
+    let start = Instant::now();
+    let handles = gups.start_workers()?;
+    for w in handles {
+        w.join().expect("failed wating for worker threads");
+    }
+    info!("shifted iteration took: {:?}", start.elapsed());
+
+    Ok(())
+}
---
+++ tools/gups/src/lib.rs
@@ -0,0 +1,209 @@
+use std::{
+    sync::{
+        atomic::{AtomicBool, AtomicUsize, Ordering},
+        Arc, Barrier,
+    },
+    thread::{self, sleep, JoinHandle},
+    time::{Duration, Instant},
+};
+
+use anyhow::Result;
+use rand::{distributions::Distribution, thread_rng};
+use tracing::info;
+
+#[derive(Debug, Default)]
+pub struct GupsSharedContex {
+    /// Total time in ns spent perform gups updates
+    elapsed_ns: AtomicUsize,
+    /// How many updates finished currently
+    finished: AtomicUsize,
+    /// Signal stats thread to exit
+    should_exit: AtomicBool,
+}
+
+#[derive(Debug, Default, Clone, Copy)]
+pub struct GupsCommonContex<D: Distribution<usize>> {
+    /// Number of worker threads
+    threads: usize,
+    /// Number of updates per thread
+    updates: usize,
+    /// Length of the entire memory region
+    len: usize,
+    /// Granularity of each update
+    granularity: usize,
+    /// Element index distribution
+    distribution: D,
+}
+
+#[derive(Debug)]
+pub struct Gups<D: Distribution<usize>> {
+    /// Mmapped memory region to perform updates
+    memory: memmap::MmapMut,
+    /// Common parameters
+    common: GupsCommonContex<D>,
+    /// Shared states for sync across threads
+    shared: Arc<GupsSharedContex>,
+    /// stats thread handle
+    stats_handle: Option<JoinHandle<()>>,
+}
+
+impl<D: Distribution<usize> + Clone + Send + 'static> Gups<D> {
+    pub fn new(
+        threads: usize,
+        updates: usize,
+        len: usize,
+        granularity: usize,
+        distribution: D,
+    ) -> Result<Self> {
+        let start = Instant::now();
+        let memory = memmap::MmapOptions::new().len(len).map_anon()?;
+        info!("mmap init took: {:?}", start.elapsed());
+        info!(
+            "global memory region [{:?}, {:?}) len 0x{:x}",
+            memory.as_ptr(),
+            memory.as_ptr().wrapping_add(memory.len()),
+            memory.len(),
+        );
+        let mut gups = Self {
+            memory,
+            common: GupsCommonContex {
+                threads,
+                updates,
+                len,
+                granularity,
+                distribution,
+            },
+            shared: Arc::new(GupsSharedContex {
+                elapsed_ns: AtomicUsize::new(0),
+                finished: AtomicUsize::new(0),
+                should_exit: AtomicBool::new(false),
+            }),
+            stats_handle: None,
+        };
+        let handle = gups.start_stats()?;
+        gups.stats_handle.replace(handle);
+        Ok(gups)
+    }
+
+    fn start_stats(&self) -> Result<JoinHandle<()>> {
+        let shared = self.shared.clone();
+        let f = move || {
+            let mut prev = shared.finished.load(Ordering::SeqCst);
+            let mut start = Instant::now();
+            while !shared.should_exit.load(Ordering::Relaxed) {
+                sleep(Duration::from_secs(1));
+                let current = shared.finished.load(Ordering::SeqCst);
+                let now = Instant::now();
+                // giga updates per second == updates per nano second
+                let gups = (current - prev) as f64 / (now - start).as_nanos() as f64;
+                info!("last second gups: {gups}");
+                prev = current;
+                start = now;
+            }
+        };
+        let handle = thread::Builder::new()
+            .name("gups stats".to_string())
+            .spawn(f)?;
+        Ok(handle)
+    }
+
+    pub fn start_workers(&mut self) -> Result<Vec<JoinHandle<()>>> {
+        let fire = Arc::new(Barrier::new(self.common.threads));
+        let goal = Arc::new(Barrier::new(self.common.threads));
+        let handles: std::result::Result<Vec<_>, _> = self
+            .memory
+            .chunks_exact_mut(self.common.len / self.common.threads)
+            .enumerate()
+            .map(|(tid, thread_mem)| {
+                let mut thread_mem = thread_mem.to_owned();
+                let common = self.common.clone();
+                let shared = self.shared.clone();
+                let fire = fire.clone();
+                let goal = goal.clone();
+                info!(
+                    "thread {tid} memory region [{:?}, {:?}) len 0x{:x}",
+                    thread_mem.as_ptr(),
+                    thread_mem.as_ptr().wrapping_add(thread_mem.len()),
+                    thread_mem.len()
+                );
+                let f = move || {
+                    // all threads should start together
+                    fire.wait();
+                    let start = Instant::now();
+                    let mut read = vec![0; common.granularity];
+                    for (finished, i) in common
+                        .distribution
+                        .sample_iter(&mut thread_rng())
+                        .take(common.updates)
+                        .enumerate()
+                    {
+                        let elem =
+                            &mut thread_mem[i * common.granularity..(i + 1) * common.granularity];
+                        // update
+                        read.copy_from_slice(elem);
+                        elem.copy_from_slice(&read);
+                        if finished % 10000 == 0 {
+                            shared.finished.fetch_add(10000, Ordering::SeqCst);
+                        }
+                    }
+                    goal.wait();
+                    if tid == 0 {
+                        // only the last thread should do the book-keeping
+                        let elapsed = start.elapsed();
+                        info!("iteration took {elapsed:?}");
+                        let gups =
+                            (common.updates * common.threads) as f64 / elapsed.as_nanos() as f64;
+                        info!("iteration gups {gups}");
+                        shared
+                            .elapsed_ns
+                            .fetch_add(elapsed.as_nanos() as _, Ordering::SeqCst);
+                    }
+                };
+                thread::Builder::new()
+                    .name(format!("gups worker {tid}"))
+                    .spawn(f)
+            })
+            .collect();
+
+        Ok(handles?)
+    }
+}
+
+impl<D: Distribution<usize>> Gups<D> {
+    /// TODO: support changing distribution online
+    pub fn reset_distribution(&mut self, distribution: D) {
+        self.common.distribution = distribution;
+    }
+
+    fn signal_exit(&self) {
+        self.shared.should_exit.store(true, Ordering::Relaxed);
+    }
+}
+
+impl<D: Distribution<usize>> Drop for Gups<D> {
+    fn drop(&mut self) {
+        self.signal_exit();
+        if let Some(h) = self.stats_handle.take() {
+            h.join().expect("failed waiting for stats thread to exit");
+        }
+        let finished = self.shared.finished.load(Ordering::SeqCst);
+        let elapsed_ns = self.shared.elapsed_ns.load(Ordering::SeqCst);
+        let gups = finished as f64 / elapsed_ns as f64;
+        info!("overall gups: {gups}");
+    }
+}
+
+// pub fn add(left: usize, right: usize) -> usize {
+//     left + right
+// }
+
+// #[cfg(test)]
+// mod tests {
+//     use super::*;
+
+//     #[test]
+//     fn it_works() {
+//         let result = add(2, 2);
+//         assert_eq!(result, 4);
+//     }
+// }
