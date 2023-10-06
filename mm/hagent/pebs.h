8f8d0dec5654a6dcaa47b086bd57a51ac576ae77
Junliang HU <jlhu@cse.cuhk.edu.hk>
2023-10-06 11:49:06 +0800

    mm/hagent: rearrange header includes

--- mm/hagent/pebs.h
+++ mm/hagent/pebs.h
@@ -3,13 +3,12 @@
 
 #include <linux/perf_event.h>
 
-#include "hagent.h"
-
 struct hagent_sample {
 	unsigned long nth, va, latency, pa;
 	// unsigned pid, tid;
 };
 
+struct hagent_info;
 void hagent_drain_pebs(struct hagent_info *info);
 
 #endif
--- mm/hagent/sdh.h
+++ mm/hagent/sdh.h
@@ -1,8 +1,6 @@
 #ifndef __MM_HAGENT_SDH_H
 #define __MM_HAGENT_SDH_H
 
-#include "rheap.h"
-
 struct sdh;
 struct sdh *sdh_new(unsigned long w, unsigned long d, unsigned long k);
 void sdh_drop(struct sdh *sdh);
